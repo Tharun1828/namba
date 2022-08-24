4417acbfc6648b534a76d2e3ac71496a187c6080
SK Rajii࿐ <104976581+SadKidBGMZ@users.noreply.github.com>
2022-08-24 18:22:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GHSrilinks4k/Sm18filter4k.git /Sm18filter4k
+  git clone https://github.com/SadKidBGMZ/Rocky_Umar75.git /Rocky_Umar75
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone https://github.com/GHSrilinks4k/Sm18filter4k
+  git clone https://github.com/SadKidBGMZ/Rocky_Umar75
 fi
 ls
-cd /Sm18filter4k/Sm18filter4k
+cd SadKidBGMZ/Rocky_Umar75
 ls
 $PWD
 pip3 install -U -r requirements.txt
